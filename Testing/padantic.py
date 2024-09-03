345aa09a92f572cdfc9710f0a33669247f01e383
James Jones <james.jones@stfc.ac.uk>
2024-09-03 14:48:19 +0100

    Update padantic.py

--- Testing/padantic.py
+++ Testing/padantic.py
@@ -2,14 +2,14 @@ import sys
 sys.path.append('..')
 from PAdantic.PAdantic import PAdantic
 
-# machine = PAdantic(layout_file='Machines/CLARA/layouts.yaml', section_file='Machines/CLARA/sections.yaml', yaml_dir='Machines/CLARA/YAML')
-# print(machine['CLA-S07-DIA-CDR-01'])
-# print(machine.elements_between(
-#         start='CLA-S05-MAG-QUAD-01', end='CLA-SP2-DIA-SCR-02',
-#         element_type=None))
+machine = PAdantic(layout_file='Machines/CLARA/layouts.yaml', section_file='Machines/CLARA/sections.yaml', yaml_dir='Machines/CLARA/YAML')
+print(machine['CLA-S07-DIA-CDR-01'])
+print(machine.elements_between(
+        start='CLA-S05-MAG-QUAD-01', end='CLA-SP2-DIA-SCR-02',
+        element_type=None))
 
-from PAdantic.Importers.SimFrame_Loader import SF_files, read_SimFrame_YAML, get_SimFrame_PV
-from PAdantic.Exporters.YAML import export_as_yaml, export_machine
+# from PAdantic.Importers.SimFrame_Loader import SF_files, read_SimFrame_YAML, get_SimFrame_PV
+# from PAdantic.Exporters.YAML import export_as_yaml, export_machine
 # machine = MachineModel(layout_file='Machines/CLARA/layouts.yaml', section_file='Machines/CLARA/sections.yaml')
 # for f in SF_files:
 #     elem = read_SimFrame_YAML(f)
