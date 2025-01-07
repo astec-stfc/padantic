b25e8f714e72e8b1484893ffcba6e45f94f3e555
James Jones <james.jones@stfc.ac.uk>
2025-01-07 12:13:00 +0000

    Update padantic.py

--- Testing/padantic.py
+++ Testing/padantic.py
@@ -9,5 +9,19 @@ machine = PAdantic(
     yaml_dir="Machines/CLARA/YAML/",
 )
 
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+
+def unique_list(a_list):
+    seen = list()
+    for x in flatten(a_list):
+        if x not in seen:
+            seen.append(x)
+    return seen
+
+
 machine.default_layout = "CLARA"
-print(machine.get_all_position_diagnostics(path='C2V'))
+abs_all_position_diagnostics = unique_list([machine.get_all_position_diagnostics(path=p) for p in machine.lattices])
+print([machine.elements[e] for e in abs_all_position_diagnostics[:1]])
