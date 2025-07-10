584f59d32efba8e519a78545abcb290b0328b8df
James Jones <james.jones@stfc.ac.uk>
2025-07-10 10:27:51 +0100

    package files

--- setup.py
+++ setup.py
@@ -7,20 +7,13 @@ with open("README.md", "r") as readme_file:
 requirements = ["pydantic>=2", "pyyaml>=6", "numpy>=1.26"]
 
 setup(
-    name="PAdantic",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author="James Jones",
-    author_email="james.jones@stfc.ac.uk",
-    description="A pydantic model for particle accelerators",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://gitlab.stfc.ac.uk/jkj62/padantic",
     packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
-    package_data={"PAdantic": ["PV_Values.yaml"]},
+    package_data={
+        "PAdantic": [
+            "Machines/**/*.yaml",
+            "PV_Values.yaml",
+        ]
+    },
 )
